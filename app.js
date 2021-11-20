63246753f64a8423257f0d43fc41aa0549eb1187
Anushka Engavle <83860689+anushkaengavle@users.noreply.github.com>
2021-11-21 00:16:30 +0530

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var txtInput = document.querySelector("#txt-Input");
+var btnConvert = document.querySelector("#btn-Convert");
+var txtOutput = document.querySelector("#txt-Output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(inputText){
+    return serverURL + "?text=" + inputText
+}
+
+function errorHandler(error) {
+    console.log("error, bad request", error)
+    alert("No response from server, try again")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var convertedText = json.contents.translated;
+        txtOutput.innerText = convertedText;
+    })
+    .catch(errorHandler)
+};
+
+
+btnConvert.addEventListener("click", clickHandler);
